dd368990267906a4bdbf4b7b37cf61f57eaf00e3
BigPapa <educarezenzi@gmail.com>
2021-03-26 16:24:18 +0200

    My Project

---
+++ MusicApp/Migration/20201128204926_AddDvdToDb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicStoreProject.Migrations
+{
+    public partial class AddDvdToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Musis",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<int>(type: "int", nullable: false),
+                    Catergory = table.Column<int>(type: "int", nullable: false),
+                    Age = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musis", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Musis");
+        }
+    }
+}
---
+++ MusicApp/Models/Dvd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApp.Models
+{
+    public class Dvd
+    { 
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        public int Catergory { get; set; }
+        public DateTime Age { get; set; }
+    }
+}
